e92481a053cc6e539b7e76f8477f6553f9bd950c
masahiro.tsuji <ma.cag08hsp02@gmail.com>
2019-11-13 03:10:26 +0900

    Add showServlet

--- src/controllers/IndexServlet.java
+++ src/controllers/IndexServlet.java
@@ -41,7 +41,7 @@ public class IndexServlet extends HttpServlet {
 
         request.setAttribute("tasks", tasks);
 
-        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/view/tasks/index.jsp");
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/index.jsp");
         rd.forward(request, response);
     }
 
