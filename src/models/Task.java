209aefa7a1b01527b41339ec571eace49cf26dd2
masahiro.tsuji <ma.cag08hsp02@gmail.com>
2019-11-28 17:46:33 +0900

    Add pagination

--- src/models/Task.java
+++ src/models/Task.java
@@ -13,71 +13,67 @@ import javax.persistence.Table;
 
 @Entity
 @NamedQueries({
-    @NamedQuery(
-        name="getAllTasks",
-        query="SELECT m FROM Task AS m ORDER BY m.id DESC"
-        )
+        @NamedQuery(name = "getAllTasks", query = "SELECT m FROM Task AS m ORDER BY m.id DESC"),
+        @NamedQuery(name = "getTasksCount", query = "SELECT COUNT(m) FROM Task AS m")
 })
-@Table(name="tasks")
+@Table(name = "tasks")
 public class Task {
-@Id
-@Column(name="id")
+    @Id
+    @Column(name = "id")
 
-@GeneratedValue(strategy=GenerationType.IDENTITY)
-private Integer id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
 
-@Column(name = "title", length = 255, nullable = false)
-private String title;
+    @Column(name = "title", length = 255, nullable = false)
+    private String title;
 
-@Column(name = "content",length = 255, nullable = false)
-private String content;
+    @Column(name = "content", length = 255, nullable = false)
+    private String content;
 
-@Column(name = "created_at",nullable = false)
-private Timestamp created_at;
+    @Column(name = "created_at", nullable = false)
+    private Timestamp created_at;
 
-@Column(name = "updated_at",nullable = false)
-private Timestamp updated_at;
+    @Column(name = "updated_at", nullable = false)
+    private Timestamp updated_at;
 
-public Integer getId() {
-    return id;
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Timestamp getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(Timestamp created_at) {
+        this.created_at = created_at;
+    }
+
+    public Timestamp getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(Timestamp updated_at) {
+        this.updated_at = updated_at;
+    }
+
 }
-
-public void setId(Integer id) {
-    this.id = id;
-}
-
-public String getTitle() {
-    return title;
-}
-
-public void setTitle(String title) {
-    this.title = title;
-}
-
-public String getContent() {
-    return content;
-}
-
-public void setContent(String content) {
-    this.content = content;
-}
-
-public Timestamp getCreated_at() {
-    return created_at;
-}
-
-public void setCreated_at(Timestamp created_at) {
-    this.created_at = created_at;
-}
-
-public Timestamp getUpdated_at() {
-    return updated_at;
-}
-
-public void setUpdated_at(Timestamp updated_at) {
-    this.updated_at = updated_at;
-}
-
-
-}
-
